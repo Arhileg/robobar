e5dfa86d9430b1bfda615befae91f15242a7affe
Arhileg <olegdiablo@gmail.com>
2021-05-31 19:30:04 +0300

    Make models Drink, Operation

--- bar/models.py
+++ bar/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 import uuid
 
+
 class Drink(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
+    name = models.CharField(primary_key=True, max_length=50)
     quantity = models.FloatField(null=True, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    
+class Operation(models.Model):
+    drink = models.ForeignKey(Drink, on_delete=models.PROTECT)
+    data = models.DateTimeField()
+    operation = models.CharField(max_length=10)
+    amount = models.IntegerField()
+    price = models.FloatField()
